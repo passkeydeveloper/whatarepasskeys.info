a1f90cc57e0d3b7cfa6103491f1d4f092ca7b917
Tim Cappalli <tim@cloudauth.dev>
2026-09-17 16:50:01 -0400

    Some final content tweaks (#31)
    * Add GitHub to footer
    
    * Lots of tweaks
    
    * Tweak
    
    * Tweak Plausible scripts

--- functions/metrics/event.js
+++ functions/metrics/event.js
@@ -1,6 +1,3 @@
-// Proxies Plausible event submissions through our own domain. Cloudflare
-// forwards the visitor's real IP to the upstream fetch automatically.
-// See: https://plausible.io/docs/proxy/guides/cloudflare
 export async function onRequest(context) {
   const request = new Request(context.request);
   request.headers.delete('cookie');
